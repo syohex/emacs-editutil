3f97f3b08515e6b28c10e0b29c6404efbaea4a8a
Syohei YOSHIDA <syohex@gmail.com>
2014-10-16 21:16:39 +0900

    Move ghc utility from init configuration file

--- helm-editutil.el
+++ helm-editutil.el
@@ -202,6 +202,29 @@
         :default (thing-at-point 'symbol)
         :buffer "*Helm HyperSpec*"))
 
+(defvar helm-editutil--ghc-mod-source
+  '((name . "GHC Browse Documennt")
+    (init . helm-editutil--ghc-mod-init)
+    (candidates-in-buffer)
+    (candidate-number-limit . 9999)
+    (action . helm-editutil--ghc-mod-action-display-document)))
+
+(defun helm-editutil--ghc-mod-init ()
+  (with-current-buffer (helm-candidate-buffer 'global)
+    (unless (process-file "ghc-mod" nil t nil "list")
+      (error "Failed 'ghc-mod list'"))))
+
+(defun helm-editutil--ghc-mod-action-display-document (candidate)
+  (let ((pkg (ghc-resolve-package-name candidate)))
+    (if (and pkg candidate)
+        (ghc-display-document pkg candidate nil)
+      (error (format "Not found %s(Package %s)" candidate pkg)))))
+
+;;;###autoload
+(defun helm-editutil-ghc-browse-document ()
+  (interactive)
+  (helm :sources '(helm-editutil--ghc-mod-source) :buffer "*helm-ghc-document*"))
+
 (provide 'helm-editutil)
 
 ;;; helm-editutil.el ends here
