befd0637ee6db34646a20a944412b5ef2c217488
Syohei YOSHIDA <syohex@gmail.com>
2014-10-20 00:19:17 +0900

    Add browser command tests

---
+++ test/test-browser-utility.el
@@ -0,0 +1,34 @@
+;;; test-browser-utility.el --- test for browser utility
+
+;; Copyright (C) 2014 by Syohei YOSHIDA
+
+;; Author: Syohei YOSHIDA <syohex@gmail.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'ert)
+
+(ert-deftest git-github-url ()
+  "editutil--git-github-url"
+  (let ((got (editutil--git-github-url "origin" "master")))
+    (string= got "https://github.com/syohex/emacs-editutil"))
+
+  (let ((got (editutil--git-github-url "origin" "master")))
+    (string= got "https://github.com/syohex/emacs-editutil")))
+
+;;; test-browser-utility.el ends here
