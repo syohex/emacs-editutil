fdc334be10e2df0da3921fa3d35b1e2b7152bf8a
Shohei YOSHIDA <syohex@gmail.com>
2025-12-07 01:25:21 +0900

    add char move utilities

--- editutil.el
+++ editutil.el
@@ -247,6 +247,24 @@
       (skip-chars-backward " \t\n")
       (skip-chars-backward "^ \t\n"))))
 
+(defun editutil-next-char (ch)
+  (interactive
+   (list (read-char)))
+  (let ((orig (point)))
+    (forward-char 1)
+    (if (search-forward (char-to-string ch) (line-end-position) t)
+        (backward-char 1)
+      (goto-char orig))))
+
+(defun editutil-previous-char (ch)
+  (interactive
+   (list (read-char)))
+  (let ((orig (point)))
+    (when (= (char-after) ch)
+      (backward-char 1))
+    (unless (search-backward (char-to-string ch) (line-beginning-position) t)
+      (goto-char orig))))
+
 (defun editutil-other-window (arg)
   (interactive "p")
   (when (one-window-p)
@@ -877,6 +895,8 @@
   ;; search-map
   (define-key search-map (kbd "l") #'editutil-next-word)
   (define-key search-map (kbd "h") #'editutil-previous-word)
+  (define-key search-map (kbd "f") #'editutil-next-char)
+  (define-key search-map (kbd "b") #'editutil-previous-char)
   (define-key search-map (kbd "SPC") #'register-to-point)
   (define-key search-map (kbd "j") #'jump-to-register)
   (define-key search-map (kbd "i") #'insert-register)
